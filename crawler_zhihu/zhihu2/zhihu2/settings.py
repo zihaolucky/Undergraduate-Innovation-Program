2a515eeefbeaa60b7e38fe4cb15eb0165c46bbc6
Zihao Zheng <zihaolucky@gmail.com>
2013-11-05 00:25:07 +0800

    抓取同一话题下的精华帖

---
+++ crawler_zhihu/zhihu2/zhihu2/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for zhihu2 project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'zhihu2'
+
+SPIDER_MODULES = ['zhihu2.spiders']
+NEWSPIDER_MODULE = 'zhihu2.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'zhihu2 (+http://www.yourdomain.com)'
---
+++ crawler_zhihu/zhihu2/zhihu2/spiders/zhihu2.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.http import Request
+from scrapy.conf import settings
+ 
+import codecs
+ 
+#使用UTF-8格式保存文件
+fp = codecs.open('record.txt', 'w', 'utf-8')
+ 
+class ZhihuSpider(BaseSpider):
+    #设置爬虫名称
+    name = "zhihu2"
+    #设置起始URL列表
+    start_urls = ["http://www.zhihu.com/topic/19553176/top-answers"]
+ 
+    def parse(self, response):
+        req = []
+        hxs = HtmlXPathSelector(response)
+        #通过XPath选出同一话题下精华帖的URL
+        page_urls =  hxs.select('//div[@id="zh-topic-top-page-list"]//h2//@href').extract()
+        print 'page_urls =', page_urls
+        for url in page_urls:
+            #构建新的URL
+            new_url = "http://www.zhihu.com" + url
+            print "[parse]new_url = %s" % (new_url)
+            #创建对应的页面的Request对象，设定回调函数为parse_cat，利用parse_cat处理返回的页面
+            r = Request(new_url, callback=self.parse_question_page)
+            req.append(r)
+        return req
+        
+    def parse_question_page(self, response):
+        req = []
+        hxs = HtmlXPathSelector(response)
+        #通过XPath选出同一话题下精华帖的URL
+        answer_urls =  hxs.select('//span[@class="answer-date-link-wrap"]//@href').extract()
+        print 'answer_urls =', answer_urls
+        
+        for url in answer_urls:
+            #构建新的URL
+            new_url = "http://www.zhihu.com" + url
+            print "[parse]new_url = %s" % (new_url)
+            #创建对应的页面的Request对象，设定回调函数为parse_cat，利用parse_cat处理返回的页面
+            r = Request(new_url, callback=self.parse_answer_page)
+            req.append(r)
+        return req
+ 
+    def parse_answer_page(self, response):
+        hxs = HtmlXPathSelector(response)
+        #利用XPath抽取出线路名称:line_name        
+        first_answer_name = hxs.select('//div[@class="zm-item-answer-author-info"]//h3/a[2]/text()').extract()
+        #利用XPath抽取路线中经过的站点名称:route
+        comment_name = hxs.select('//div[@class="zm-comment-hd"]/a/text()').extract()
+        #结果写入到记录的文件之中
+        topic_name = hxs.select('//h2[@class="zm-item-title zm-editable-content"]/a/text()').extract()
+        
+        for i in range(len(first_answer_name)):
+            fp.write( topic_name[0].strip() + ',' + first_answer_name[i].strip() )
+            fp.write('\r\n')
+        
+        #二级评论
+        for i in range(len(comment_name)):
+            fp.write( first_answer_name[0].strip() + ',' + comment_name[i].strip() )
+            fp.write('\r\n')
+        
+        print "\n"
+        print "topic name:",topic_name[0].strip()
+        print "\n"
+        for i in range(len(comment_name)):
+            print comment_name[i]
+        for j in range(len(first_answer_name)):
+            print first_answer_name[j]
+        print '--------------------------------------------------------------'
+        print "\n"
