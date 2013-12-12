a379f5e53fc1cbbd0648663bfe97ec0072b39a11
Zihao Zheng <zihaolucky@gmail.com>
2013-12-12 14:11:07 +0800

    添加"下一页"功能

--- crawler_zhihu/zhihu2/zhihu2/spiders/zhihu2.py
+++ crawler_zhihu/zhihu2/zhihu2/spiders/zhihu2.py
@@ -12,13 +12,18 @@ fp = codecs.open('record.txt', 'w', 'utf-8')
 class ZhihuSpider(BaseSpider):
     #设置爬虫名称
     name = "zhihu2"
-    #设置起始URL列表
+    #设置起始URL列表,it must be a list.
     start_urls = ["http://www.zhihu.com/topic/19553176/top-answers"]
+    for i in range(2,20):
+        page = "http://www.zhihu.com/topic/19553176/top-answers?page="+ str(i)
+        start_urls.append(page)
+    
  
     def parse(self, response):
         req = []
         hxs = HtmlXPathSelector(response)
         #通过XPath选出同一话题下精华帖的URL
+        global page_urls
         page_urls =  hxs.select('//div[@id="zh-topic-top-page-list"]//h2//@href').extract()
         print 'page_urls =', page_urls
         for url in page_urls:
@@ -50,18 +55,18 @@ class ZhihuSpider(BaseSpider):
         hxs = HtmlXPathSelector(response)
         #利用XPath抽取出线路名称:line_name        
         first_answer_name = hxs.select('//div[@class="zm-item-answer-author-info"]//h3/a[2]/text()').extract()
-        #利用XPath抽取路线中经过的站点名称:route
+        #
         comment_name = hxs.select('//div[@class="zm-comment-hd"]/a/text()').extract()
         #结果写入到记录的文件之中
         topic_name = hxs.select('//h2[@class="zm-item-title zm-editable-content"]/a/text()').extract()
         
         for i in range(len(first_answer_name)):
-            fp.write( topic_name[0].strip() + ',' + first_answer_name[i].strip() )
+            fp.write( topic_name[0].strip() + ';' + first_answer_name[i].strip() )
             fp.write('\r\n')
         
         #二级评论
         for i in range(len(comment_name)):
-            fp.write( first_answer_name[0].strip() + ',' + comment_name[i].strip() )
+            fp.write( first_answer_name[0].strip() + ';' + comment_name[i].strip() )
             fp.write('\r\n')
         
         print "\n"
