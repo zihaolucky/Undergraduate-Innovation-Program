dc85622e22ca321fcdda3c150f5122eddf4b9bec
Zihao Zheng <zihaolucky@gmail.com>
2014-01-25 22:29:38 +0800

    followers用户信息

--- crawler_zhihu/chrome_js/followers.js
+++ crawler_zhihu/chrome_js/followers.js
@@ -1,15 +1,10 @@
 //用逗号分隔用户名
-var userarray = "banlon";
-//回答数限制
-var answerlimit = 0;
-//赞同数限制
-var agreelimit = 0;
-//关注者数限制
-var followerlimit = 0;
- 
+var userarray = "shek";
+
 var users = userarray.split(',');
 var usercursor = 0;
-var result = new Array();
+// insert the followers_number
+var result = new Array(users,followers_number);
 var showtable = true;
 var cardcount = 0;
  
@@ -18,7 +13,7 @@ function showresult() {
     $("#switchshowtable").show(0);
     $("#sorttype").show(0);
     var rsdiv = $("#result");
-    if (showtable) {
+    if (!showtable) {
         var tablehtm = "<table border='1' cellpadding='2' style='border-collapse: collapse;'><tr><td>编号</td><td>用户名</td><td>关注者</td><td>提问</td><td>回答</td><td>赞同</td></tr>";
         for (i in result) {
             tablehtm += "<tr><td>" + (parseInt(i) + 1) + "</td><td><a href='/people/" + result[i].id + "/' target='_blank'>" + result[i].name + "</a></td><td>" + result[i].follower + "</td><td>" + result[i].ask + "</td><td>" + result[i].answer + "</td><td>" + result[i].agree + "</td></tr>";
@@ -46,7 +41,7 @@ function loadmore() {
         var total = content.find(".zm-profile-side-following strong").html();
         cardcount = content.find('.zm-profile-card .zm-list-content-medium').length;
         showmsg("正在加载" + name + "的关注者:" + cardcount + "/" + total + "... <img style='vertical-align: text-bottom;' src='http://static.zhihu.com/static/img/spinner/grey-loading.gif'/>");
-        setTimeout(loadmore, 2000);
+        setTimeout(loadmore, 1000);
     }
 }
  
@@ -60,7 +55,6 @@ function showratio() {
         var ask = Number(detail.eq(0).children().eq(1).html().split(' ')[0]);
         var answer = Number(detail.eq(0).children().eq(2).html().split(' ')[0]);
         var agree = Number(detail.eq(0).children().eq(3).html().split(' ')[0]);
-		//控制部分,将获得的所有信息都一并存入;调用addresult()函数
         if (true) {
             var r = new Object();
             r.name = name;
@@ -69,11 +63,10 @@ function showratio() {
             r.ask = ask;
             r.agree = agree;
             r.answer = answer;
-            //r.ratio = (agree / answer).toFixed(2);
             addresult(r);
         }
     });
-    //sortresult();
+    //sortresult();  uncomment this if you want to sort result.
     showresult();
     usercursor++;
     loaduser();
@@ -91,17 +84,20 @@ function loaduser() {
  
 function addresult(r) {
     var exist = false;
-    for (i in result) { if (r.id == result[i].id) { exist = true; break; } }
+    //for (i in result) { if (r.id == result[i].id) { exist = true; break; } }
     if (!exist) result.push(r);
 }
- 
+
+// This part is invalid here.
 function sortresult() {
     if (result.length > 0) {
         var type = $("#sorttype").val();
         switch (type) {
-            //case "ratio":
-            //    result = result.sort(function (a, b) { return b.ratio - a.ratio; });
-            //    break;
+			/*
+			case "ratio":
+                result = result.sort(function (a, b) { return b.ratio - a.ratio; });
+                break;
+			*/
             case "agree":
                 result = result.sort(function (a, b) { return b.agree - a.agree; });
                 break;
@@ -121,7 +117,7 @@ function sortresult() {
 }
  
 $("body").prepend('<div id="mask" style="width:100%;height:100%;top:0px;left:0px;position:fixed;z-index: 998;background-color: rgba(0, 0, 0, 0.4);text-align:center;"><div id="container" style="width:600px;height:400px;margin:80px auto 0px auto;position: relative;z-index: 999; padding: 5px;"><iframe id="tempframe" style="width:1px;height:1px;top:-999px;left:-999px;position:absolute;"></iframe><div id="msg" style="height: 30px;background-color: #C4D299;line-height: 30px;text-align: left;padding-left: 5px;"></div><div id="result" style="height: 350px;background-color: #F0F0F0;text-align: left;padding: 5px;margin-top: 5px;overflow-y: auto;"></div><input id="switchshowtable" style="display:none;position: absolute;width: 100px;top: 10px;right: 25px;" type="button" value="改为逗号分隔"/><select id="sorttype" name="sorttype" style="display:none;position: absolute;width: 100px;top: 45px;right: 25px;"><option value="ratio" selected>赞同/回答比</option><option value="agree">赞同</option><option value="answer">回答</option><option value="ask">提问</option><option value="follower">关注</option></select></div></div>');
-$("#switchshowtable").click(function () { showtable = !showtable; $(this).val(showtable ? "改为逗号分隔" : "改为表格显示"); showresult(); });
+$("#switchshowtable").click(function () { showtable = !showtable; $(this).val(showtable ? "改为表格分隔" : "改为逗号显示"); showresult(); });
 $("#sorttype").change(function () { sortresult(); showresult(); });
 $("#tempframe").load(function () { loadmore(); });
 loaduser();
